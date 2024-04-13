e85b6bee38f334d470a7fb89c83babb052b10aff
C <chrismatusemail@gmail.com>
2024-04-13 22:29:37 +0100

    Change branding.

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import DataContext from "../context/dataContext";
+import mainLogo from "../logo192.png";
 
 const Start = () => {
   const { startQuiz, showStart } = useContext(DataContext);
@@ -10,8 +11,9 @@ const Start = () => {
     >
       <div className="container">
         <div className="row vh-100 align-items-center justify-content-center">
-          <div className="col-lg-8">
-            <h1 className="fw-bold mb-4">Basic React JS Quiz</h1>
+          <div className="col-lg-8 pb-5">
+            <img src={mainLogo} alt="Lingfinity logo" className="w-25" />
+            <h1 className="fw-bold mb-4">Lingfinity</h1>
             <button
               onClick={startQuiz}
               className="btn px-4 py-2 bg-light text-dark fw-bold"
